469f09fbd7314a39c862f38be87aafeaa0e2df14
Jauri da Cruz Junior <jauri.dev@gmail.com>
2023-01-24 11:47:12 -0300

    stack view sample

---
+++ StackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Jauri da Cruz Junior on 24/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
